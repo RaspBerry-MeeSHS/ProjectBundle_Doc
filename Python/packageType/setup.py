70f2c9b54c99c7014c3f76acc99325dd33ddf4c4
Amatsu <amatsukuyom1port@gmail.com>
2021-05-12 15:37:31 +0200

    Format Type d'un package Python

---
+++ Python/packageType/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(name='',
+      version='',
+      author='',
+      description='',
+      long_description='',
+      # https://pypi.org/pypi?%3Aaction=list_classifiers
+      url='',
+      classifiers=[
+            '',
+      ],
+      author_email='',
+      package_data={
+            # If any package contains *.txt or *.rst files, include them:
+            "": ["*.txt", "*.rst"],
+            # And include any *.msg files found in the "hello" package, too:
+            "hello": ["*.msg"],
+      },
+      license='',
+      packages=[''],
+      python_requires='',
+      install_requires=[
+          '',
+      ],
+      dependency_links=[
+          ''],
+      zip_safe=False)
---
+++ Python/packageType/sous_module/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+    Docstring du sous_module
+"""
+
+from .programmeDuSousModule import ClasseAFaire, fonctionPasDeClass
+
+
+if __name__ == '__main__':
+    pass
